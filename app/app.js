044e8c43bb2f116f5bfb24d7c6391bb91f92bc9d
Christine Al-Thifairy <chris@jzoo.com>
2018-10-14 11:16:30 +1100

    Code cleanup

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+// @flow
+
 import * as React from 'react';
 
 import HelloPage from 'app/pages/hello';
--- app/index.js
+++ app/index.js
@@ -1,9 +1,14 @@
+// @flow
+
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import 'babel-polyfill';
+import '@babel/polyfill';
 
 import App from './app';
 
 const rootEl = document.getElementById('root');
-
-ReactDOM.render(<App />, rootEl);
+if (rootEl) {
+  ReactDOM.render(<App />, rootEl);
+} else {
+  throw new Error('rootEl not found in page.');
+}
